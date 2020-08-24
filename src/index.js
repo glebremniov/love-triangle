b45207855d992b6df6e5b1290e403805f4885aab
glebremiov <remniov.work@gmail.com>
2020-08-24 21:23:15 +0300

    Fast working implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
+const checkIfElementExists = (array = [], n) => {
+    return array.length >= n;
+};
+
+const getLoveTrianglesCount = (preferences = []) => {
+    // your implementation
+    let loveTrianglesCount = 0;
+
+    preferences.forEach((item, index) => {
+
+        if (checkIfElementExists(preferences, item)) {
+
+            const secondLover = preferences[item - 1];
+
+            if (checkIfElementExists(preferences, secondLover)) {
+                const thirdLover = preferences[secondLover - 1]
+
+                if (thirdLover === index + 1) {
+                    loveTrianglesCount++
+                }
+            }
+        }
+
+    });
+
+    return Math.floor(loveTrianglesCount / 3);
+};
+
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = getLoveTrianglesCount;
